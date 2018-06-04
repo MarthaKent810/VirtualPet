685eef738bd488c03ba4697b764fe111984ac44c
MarthaKent810 <martybacho@gmail.com>
2018-06-03 22:16:26 -0400

    final project git push

--- VirtualPet/Program.cs
+++ VirtualPet/Program.cs
@@ -10,6 +10,71 @@ namespace VirtualPet
     {
         static void Main(string[] args)
         {
+            VirtualPet fidget = new VirtualPet("Fidget", 5, 5, 7);
+            Console.WriteLine("Thank you for adopting {0} the mini-moo", fidget.Name);
+            Console.WriteLine("She requires special care & needs to be monitored carefully");
+            Console.WriteLine("She will need food, naps & exercise if she gets too restless");
+            Console.WriteLine("You will get a warning if she gets hungry, tired or too fidgety");
+            Console.WriteLine("Type yes if you would like to take care of {0} now?", fidget.Name);
+            string userResp = Console.ReadLine().ToLower();
+
+            while (userResp.Equals("yes"))
+            {
+                fidget.Tick();
+
+                Console.WriteLine();
+                Console.WriteLine("{0} the mini-moo:", fidget.Name);
+                Console.WriteLine("Hunger is at {0} ", fidget.Hunger);
+                Console.WriteLine("Sleepiness is at {0} ", fidget.Sleep);
+                Console.WriteLine("Restlessness is at {0}", fidget.Activity);
+                if (fidget.Hunger < 3)
+                {
+                    Console.WriteLine("{0} is hungry, you need to feed her", fidget.Name);
+                }
+                if (fidget.Sleep < 3)
+                {
+                    Console.WriteLine("{0} is exhausted; she needs a nap", fidget.Name);
+                }
+                if (fidget.Activity > 10)
+                {
+                    Console.WriteLine("{0} is too restless, you need to play with her", fidget.Name);
+                }
+
+                Console.WriteLine("Select one of the options below to care for {0} ", fidget.Name);
+                Console.WriteLine("1 will let you feed {0}", fidget.Name);
+                Console.WriteLine("2 will let you give {0} a nap", fidget.Name);
+                Console.WriteLine("3 will give {0} some much needed exercise", fidget.Name);
+                int userSel = int.Parse(Console.ReadLine());
+
+                switch (userSel)
+                {
+                    case 1:
+                        fidget.Feed();
+                        Console.WriteLine("You gave {0} some food. Thank you!", fidget.Name);
+                        break;
+                    case 2:
+                        fidget.Rest();
+                        Console.WriteLine("{0} is now resting.  She'll feel much better", fidget.Name);
+                        break;
+                    case 3:
+                        fidget.Play();
+                        Console.WriteLine("{0} got some exercise. She's much happier now", fidget.Name);
+                        break;
+                    default:
+                        break;
+                }
+
+                Console.WriteLine("Type yes if you would like to take care of {0} now?", fidget.Name);
+                userResp = Console.ReadLine().ToLower();
+            }
+
         }
+
     }
 }
+
+
+
+
+    
+
---
+++ VirtualPet/VirtualPet.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualPet
+{
+    class VirtualPet
+    {   //declare fields
+        private string name;
+        private int hunger;
+        private int sleep;
+        private int activity;
+        private bool hungerAlert;
+        private bool sleepAlert;
+        private bool activityAlert;
+
+        //declare properties
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+
+        public bool HungerAlert
+        {
+            get { return this.hungerAlert; }
+            set { this.hungerAlert = value; }
+        }
+        public bool SleepAlert
+        {
+            get { return this.sleepAlert; }
+            set { this.sleepAlert = value; }
+        }
+
+        public bool ActivityAlert
+        {
+            get { return this.activityAlert; }
+            set { this.activityAlert = value; }
+        }
+
+        public int Hunger
+        {
+            get { return this.hunger; }
+            set { this.hunger = value; }
+        }
+
+        public int Sleep
+        {
+            get { return this.sleep; }
+            set { this.sleep = value; }
+        }
+
+        public int Activity
+        {
+            get { return this.activity; }
+            set { this.activity = value; }
+        }
+
+        //Declare constructors
+        public VirtualPet()
+        {
+            //default constructor
+        }
+
+        public VirtualPet(string name, int hunger, int sleep, int activity)
+        {
+            this.name = name;
+            this.hunger = hunger;
+            this.sleep = sleep;
+            this.activity = activity;
+        }
+
+        public void Tick()
+        {
+            Hunger--;
+            Sleep--;
+            Activity +=2;
+        }
+
+        public void Feed()
+        {
+            Hunger += 3;
+        }
+
+        public void Rest()
+        {
+            Sleep += 3;
+        }
+
+        public void Play()
+        {
+            Activity -= 4;
+            Hunger--;
+        }
+
+
+    }
+}
